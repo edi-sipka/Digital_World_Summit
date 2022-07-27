41ace0926c8352e26c3594adaa37d65d7f921a96
edi-sipka@gmail.com <edisipka@gmail.com>
2022-07-27 09:24:31 +0100

    Add javascript

---
+++ presenters.js
@@ -0,0 +1,44 @@
+const presenters = [
+  {
+    name: 'Ali Kennedy',
+    title: 'President of the Digital Transform Club',
+    description:
+      'He started his business journey in early childhood and hopped on the hype train of the rapidly growing Internet technology in the late ’90s.',
+    img: './Images/speaker1.jpg',
+  },
+  {
+    name: 'John Martin',
+    title: 'CEO of DigitalMania',
+    description:
+      'Besides his own businesses, he has consulted and growth-hacked such companies as Amazon, General Motors, Hewlett Packard and NBC.',
+    img: './Images/speaker3.jpg',
+  },
+  {
+    name: 'Anna Patrick',
+    title: 'CTO of Creative News',
+    description:
+      'She shares the best strategies and techniques for boosting branding, selling on social media, brand development, social media optimization, and much more.',
+    img: './Images/speaker2.jpg',
+  },
+  {
+    name: 'Landa Okyle',
+    title: 'President of Young Digital marketers',
+    description:
+      'Besides numerous books, she is also known for his podcasts and audio shows that have already exceeded 100 million downloads.',
+    img: './Images/speaker5.jpg',
+  },
+  {
+    name: 'Eliah Markley',
+    title: 'CEO of IT tips and tricks',
+    description:
+      'He consulted companies like PepsiCo, Walmart, Microsoft, and Adobe. Eliah’s favorite topic is the disruption in marketing.',
+    img: './Images/speaker4.jpg',
+  },
+  {
+    name: 'Malik Blant',
+    title: 'Executive Director of 99days',
+    description:
+      'His main specialization is word-of-mouth and viral marketing that makes your customers bring more customers.',
+    img: './Images/speaker6.jpg',
+  },
+];
